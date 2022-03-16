0c45e119925cdffaaa9c329f46e7c919742ec554
J-724 <jjpernian@gmail.com>
2022-03-16 00:25:08 -0400

    Game UI created

--- js/script.js
+++ js/script.js
@@ -1,96 +1,117 @@
-console.log('Mi Ryuk!');
+const p1Choice = document.getElementById('p1-choices');
+p1Choice.addEventListener('click',getSelection);
 
-/* player choice
-computer choice
-winner
-rockPaperScissor
-new game */
+const playermsg = document.getElementById('player-msg');
+const pcmsg = document.getElementById('pc-msg');
 
-function playerChoice() {
-    let choice; 
-    do {
-        choice = window.prompt("Chosse your movement \n Rock    Paper    Scissors");
-        choice = choice.toLowerCase();
-    } while ((choice != 'rock')&&(choice != 'paper')&&(choice != 'scissors')); 
-    return choice;
+let gameFinished = false;
+let playerScore = 0;
+let computerScore = 0;
+let winner ='';
+
+function getSelection(e){
+    if(gameFinished==false){
+        const rock=document.querySelector('div[data-choice=rock]');
+        const paper=document.querySelector('div[data-choice=paper]');
+        const scissors=document.querySelector('div[data-choice=scissors]');
+        
+        if ((e.target == document.querySelector('div[data-choice=rock]'))
+            ||(e.target == document.querySelector('img[data-choice=rock]'))
+            ||(e.target == document.querySelector('p[data-choice=rock]'))){
+                
+                paper.classList.remove('selected');
+                scissors.classList.remove('selected');    
+                rock.classList.add('selected');
+                getRoundWinner('rock');
+        } else if ((e.target == document.querySelector('div[data-choice=paper]'))
+            ||(e.target == document.querySelector('img[data-choice=paper]'))
+            ||(e.target == document.querySelector('p[data-choice=paper]'))){
+                
+                rock.classList.remove('selected');
+                scissors.classList.remove('selected');
+                paper.classList.add('selected');
+                getRoundWinner('paper');
+        } else if ((e.target == document.querySelector('div[data-choice=scissors]'))
+            ||(e.target == document.querySelector('img[data-choice=scissors]'))
+            ||(e.target == document.querySelector('p[data-choice=scissors]'))) {
+                
+                rock.classList.remove('selected');
+                paper.classList.remove('selected');
+                scissors.classList.add('selected')
+                getRoundWinner('scissors');
+            }        
+    }
 }
 
 function computerChoice() {
     let choice = Math.floor(Math.random()*3 + 1);
+    
+    const rock=document.getElementById('rock-pc');
+    const paper=document.getElementById('paper-pc');
+    const scissors=document.getElementById('scissors-pc');
+    try {
+        rock.classList.remove('selected');
+        paper.classList.remove('selected');
+        scissors.classList.remove('selected');
+    } catch {}
+
     if (choice === 1) {
+        rock.classList.add('selected');
         choice = 'rock';
     } else if (choice === 2){
+        paper.classList.add('selected');
         choice = 'paper';
     } else {
+        scissors.classList.add('selected');
         choice = 'scissors';
     }
     return choice;
 }
 
-// paper wins rock
-// rock wins scissors
-// scissor wins paper
-
-function roundwinner() {
-    let player= playerChoice();
+function getRoundWinner(playerChoice){
     let computer = computerChoice();
 
-    if ((player=='paper' && computer=='rock')||(player=='rock' && computer=='scissors')||(player=='scissors' && computer=='paper')){
-        console.log ('Player choice: '+player+' Computer choice: '+computer);
-        return 'Player Wins!';
-    } else if (player==computer){
-        console.log ('Player choice: '+player+' Computer choice: '+computer);
-        return 'Tie';
+    if ((playerChoice=='paper' && computer=='rock')
+        ||(playerChoice=='rock' && computer=='scissors')
+        ||(playerChoice=='scissors' && computer=='paper')){
+        
+        playerScore++;
+        playermsg.textContent='Player Wins the round!!!';
+        pcmsg.textContent='Player Wins the round!!!';
+    } else if (playerChoice==computer){
+        playermsg.textContent='Draw';
+        pcmsg.textContent='Draw';
     } else {
-        console.log ('Player choice: '+player+' Computer choice: '+computer);
-        return 'Computer Wins!'
+        computerScore++;
+        playermsg.textContent='Computer Wins the round!!!';
+        pcmsg.textContent='Computer Wins the round!!!';
     }
+    return setScore(playerScore, computerScore);
 }
 
-function rockPaperScissor() {
-    //The game will be set to play for 3 out 5
-    let winner;
-    let playerWins=0;
-    let computerWins=0;
-    let tie=0;
+function setScore (playerScore, computerScore) {
+    const playerCounter = document.getElementById('playerScore').textContent = `${playerScore}`;
+    const computerCounter = document.getElementById('pcScore').textContent = `${computerScore}`;
 
-    for (let i=0;i<5;i++){
-        winner=roundwinner();
-        console.log(winner);
-        if (winner == 'Player Wins!'){
-            playerWins++;
-            console.log('player wins: '+playerWins);
-            if (playerWins>=3){
-                console.log('The Player won the game!');
-                break;
-            }
-        } else if (winner == 'Tie'){
-            tie++;
-            console.log('Ties: '+tie);
-            if (tie>=3){
-                console.log('The game end in a Tie');
-                break;
-            }
-        } else {
-            computerWins++;
-            console.log('Computers wins: '+computerWins);
-            if (computerWins>=3){
-                console.log('The Computer won the game!');
-                break;
-            }
-        }
-
-        if ((computerWins==2)&&(playerWins==2)&&(i=5)){
-            console.log('The game end in a Tie');
-            break;
-        }
-    }
-
+    if (playerScore >= 3){
+        gameFinished = true;
+        showWinner('Player Wins !!!');
+    } else if (computerScore >= 3){
+        gameFinished = true;
+        showWinner('computer Wins !!!');
+    }    
 }
 
-rockPaperScissor();
+function showWinner (winner){
+    const msgWindow = document.querySelector('[class=overlay-msg]');
+    msgWindow.style.visibility = 'visible';
 
-function newGame() {
+    const text = document.getElementById('winmsg').textContent = `${winner}`;
 
+    const newGame = document.getElementById('newGame');
+    newGame.addEventListener('click', restart);
 }
 
+function restart () {
+    window.location.reload();
+}
